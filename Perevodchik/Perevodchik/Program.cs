2c4d266740b0a95b00da31a5c36fbdf29a1a74af
Flovers <80450422+Flovers123@users.noreply.github.com>
2021-09-04 11:04:41 +0300

    Add files via upload

---
+++ Perevodchik/Perevodchik/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Perevodchik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для перевода: ");
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a < 0)
+                    Console.WriteLine("Число должно быть больше ноля");          
+                Console.Write("Введите СС: ");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine(Convert.ToString(a, b));             
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Это не число");
+            }
+
+        }
+    }
+}
+
